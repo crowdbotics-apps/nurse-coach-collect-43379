7bd555d3fa0a65025a70a353ffb89e4b21921f91
Crowdbotics <team@crowdbotics.com>
2023-09-14 05:57:23 +0000

    Initial commit.

---
+++ backend/nurse_coach_collect_43379/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nurse_coach_collect_43379 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nurse_coach_collect_43379.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nurse-coach-collect-43379",
+  url: "https://nurse-coach-collect-43379.botics.co",
+  api: "https://nurse-coach-collect-43379.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
